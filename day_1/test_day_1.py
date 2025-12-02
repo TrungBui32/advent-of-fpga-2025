2853c50a9e3c7f73282a6b3545d749a6d215a6cc
TrungBui32 <thetrung205@gmail.com>
2025-12-02 18:43:33 +0800

    update testbench in cocotb

--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,7 +1,18 @@
 import cocotb
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.clock import Clock
 
 @cocotb.test()
 async def test(dut):
-    await Timer(1, units='ns')
+    clock = Clock(dut.clk, 10, units="ns")
+    cocotb.start_soon(clock.start())
     
+    dut.process.value = 0
+    await Timer(100, units='ns')
+    
+    dut.process.value = 1
+    
+    while not dut.finished.value:
+        await RisingEdge(dut.clk)
+    
+    print(f"Final result: {int(dut.result.value)}")

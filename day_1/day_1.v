b9b524fd62aeb6fbf961d6f84614f70cdfa569ac
TrungBui32 <thetrung205@gmail.com>
2025-12-02 15:35:46 +0800

    O(n) solution

--- day_1/day_1.v
+++ day_1/day_1.v
@@ -1,35 +1,40 @@
 module day_1();
     localparam LENGTH = 4186;
     reg [10:0] ops [0:LENGTH-1]; 
-    reg signed [31:0] dial_position = 50; 
+    reg [31:0] dial_position = 50; 
     reg [31:0] counter = 0;  
-
-    integer i, j;
+    
+    integer i;
     integer rotation_amount;
-    integer current_pos;
+    integer remaining_rotation;
 
     initial begin
         $readmemh("input.mem", ops);
         
         for(i = 0; i < LENGTH; i = i + 1) begin
             rotation_amount = ops[i][9:0];
-            current_pos = dial_position;
             
-            for(j = 0; j < rotation_amount; j = j + 1) begin
-                if(ops[i][10] == 1'b1) begin  
-                    current_pos = (current_pos + 1) % 100;
-                end else begin  
-                    current_pos = (current_pos - 1 + 100) % 100;
+            if(ops[i][10] == 1'b1) begin  
+                if(dial_position + rotation_amount >= 100) begin
+                    counter = counter + (dial_position + rotation_amount) / 100;
+                    dial_position = (dial_position + rotation_amount) % 100;
+                end else begin
+                    dial_position = dial_position + rotation_amount;
                 end
-                
-                if(current_pos == 0) begin
-                    counter = counter + 1;
+            end else begin  
+                counter = counter + rotation_amount / 100;
+                remaining_rotation = rotation_amount % 100;
+                if(dial_position != 0) begin
+                    if(dial_position <= remaining_rotation) begin
+                        counter = counter + 1;
+                        dial_position = (100 + (dial_position - remaining_rotation)) % 100;
+                    end else begin
+                        dial_position = dial_position - remaining_rotation;
+                    end
+                end else begin
+                    dial_position = 100 - remaining_rotation;
                 end
             end
-            
-            dial_position = current_pos;
         end
-        
-        $display("Password: %0d", counter);
     end
 endmodule

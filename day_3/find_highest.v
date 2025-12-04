bc00503c310795b05e6e37de23d95d494544b2b4
TrungBui32 <thetrung205@gmail.com>
2025-12-04 11:19:10 +0800

    reduce a stage, use comb instead

--- day_3/find_highest.v
+++ day_3/find_highest.v
@@ -9,11 +9,10 @@ module find_highest #(
     output reg [39:0] result_1
 );
     localparam IDLE = 3'd0;
-    localparam FILL = 3'd1;
-    localparam PRE_TRACE = 3'd2;
-    localparam TRACE = 3'd3;
-    localparam SUM = 3'd4;
-    localparam DONE = 3'd5;
+    localparam PRE_TRACE = 3'd1;    
+    localparam TRACE = 3'd2;
+    localparam SUM = 3'd3;
+    localparam DONE = 3'd4;
 
     reg [2:0] state;
     reg [3:0] digit_array [0:11];  
@@ -39,23 +38,26 @@ module find_highest #(
                 IDLE: begin
                     if(start) begin
                         i <= 0;
-                        state <= FILL;
+                        state <= PRE_TRACE;
                         temp_num <= num;
                         current_digit <= 4'd0;
                         count <= 4'd0;
                         shift <= 1'b0;
                         result <= 40'd0;
+                        digit_array[0] <= num[3:0];
+                        digit_array[1] <= num[7:4];
+                        digit_array[2] <= num[11:8];
+                        digit_array[3] <= num[15:12];
+                        digit_array[4] <= num[19:16];
+                        digit_array[5] <= num[23:20];
+                        digit_array[6] <= num[27:24];
+                        digit_array[7] <= num[31:28];
+                        digit_array[8] <= num[35:32];
+                        digit_array[9] <= num[39:36];
+                        digit_array[10] <= num[43:40];
+                        digit_array[11] <= num[47:44];
+                        temp_num <= num >> 48;
                     end
-                end
-                FILL: begin
-                    if(i < 12) begin
-                        digit_array[i] <= temp_num[3:0];
-                        temp_num <= temp_num >> 4;
-                        i <= i + 1;
-                    end else begin 
-                        state <= PRE_TRACE;
-                        i <= 0;
-                    end 
                 end
                 PRE_TRACE: begin
                     if(temp_num > 0) begin

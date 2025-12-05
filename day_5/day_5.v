378d60ceddffa837fa08256618540ced0dc00d76
TrungBui32 <thetrung205@gmail.com>
2025-12-05 22:20:43 +0800

    sequential solution for part 2

--- day_5/day_5.v
+++ day_5/day_5.v
@@ -3,39 +3,39 @@ module day_5(
     input rst,
     input start,
     output reg finished,
-    output reg [10:0] result
+    output reg [WIDTH-1:0] result
 );
     localparam NUM_RANGE = 182;
-    localparam NUM_ID = 1000;
     localparam WIDTH = 50;
 
     localparam IDLE = 3'd0;
     localparam CHECK = 3'd1;
-    localparam DONE = 3'd2;
+    localparam RANGE = 3'd2;
+    localparam SUM = 3'd3;
+    localparam DONE = 3'd4;
 
     reg [WIDTH-1:0] start_range [0:NUM_RANGE-1];
     reg [WIDTH-1:0] end_range [0:NUM_RANGE-1];
-    reg [WIDTH-1:0] ids [0:NUM_ID-1];
+    reg [NUM_RANGE-1:0] valid_range;
 
     wire [NUM_RANGE-1:0] in_range;
 
     reg [2:0] state;
 
+    integer x;
+
     initial begin
         $readmemb("start_range.mem", start_range);
         $readmemb("end_range.mem", end_range);
-        $readmemb("input.mem", ids);
+        for(x = 0; x < NUM_RANGE; x = x + 1) begin
+            valid_range[x] = 1;
+        end
     end
 
-    reg [9:0] j;
-    reg [10:0] sum;
 
-    genvar i;
-    generate
-        for(i = 0; i < NUM_RANGE; i = i + 1) begin : range_check
-            assign in_range[i] = (ids[j] >= start_range[i]) && (ids[j] <= end_range[i]);
-        end
-    endgenerate
+
+    reg [9:0] i, j, k;
+    reg [WIDTH-1:0] sum;
 
     wire is_in_range = |in_range;
 
@@ -44,23 +44,63 @@ module day_5(
             finished <= 0;
             result <= 0;
             state <= IDLE;
+            i <= 0;
             j <= 0;
+            k <= 0;
             sum <= 0;
         end else begin
             case(state)
                 IDLE: begin
                     if(start) begin
                         state <= CHECK;
+                        i <= 0;
                         j <= 0;
+                        k <= 0;
                         sum <= 0;
                     end
                 end
                 CHECK: begin
-                    if(j < NUM_ID) begin
-                        if(is_in_range) begin
-                            sum <= sum + 1;
+                    if(i < NUM_RANGE) begin
+                        if(valid_range[i] == 1'b1) begin
+                            state <= RANGE;
+                            j <= i + 1;
+                        end else begin
+                            i <= i + 1;
                         end
-                        j <= j + 1;
+                    end else begin
+                        state <= SUM;
+                    end
+                end
+                RANGE: begin
+                    if(j < NUM_RANGE) begin
+                        if(valid_range[j] == 1'b1) begin
+                            if(start_range[i] <= end_range[j] && end_range[i] >= start_range[j]) begin
+                                if(start_range[j] < start_range[i]) begin
+                                    start_range[i] <= start_range[j];
+                                end
+                                if(end_range[j] > end_range[i]) begin
+                                    end_range[i] <= end_range[j];
+                                end
+                                valid_range[j] <= 1'b0;
+                                state <= CHECK;
+                                i <= 0;
+                            end else begin
+                                j <= j + 1;
+                            end
+                        end else begin
+                            j <= j + 1;
+                        end
+                    end else begin
+                        state <= CHECK;
+                        i <= i + 1;
+                    end
+                end
+                SUM: begin
+                    if(k < NUM_RANGE) begin
+                        if(valid_range[k] == 1'b1) begin
+                            sum <= sum + (end_range[k] - start_range[k] + 1);
+                        end
+                        k <= k + 1;
                     end else begin
                         state <= DONE;
                     end

aa4302802c7f4061cb9f80337f8685fe884a8289
TrungBui32 <thetrung205@gmail.com>
2025-12-07 00:59:32 +0800

    modify input format

--- day_6/convert_input.py
+++ day_6/convert_input.py
@@ -2,28 +2,81 @@ def convert_input():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
     
-    # Process first 4 lines (numerical data)
+    param_lines = []
     for i in range(4):
-        numbers = lines[i].strip().split()
+        param_lines.append(lines[i].rstrip('\n'))
+    
+    operator_line = lines[4].rstrip('\n')
+    
+    max_length = max(len(line) for line in param_lines + [operator_line])
+    
+    for i in range(len(param_lines)):
+        param_lines[i] = param_lines[i].ljust(max_length)
+    operator_line = operator_line.ljust(max_length)
+    
+    col = 0
+    problem_count = 0
+    all_values = [[], [], [], []]
+    operators = []
+    current_problem = ['', '', '', '']
+    in_problem = False
+    
+    while col < max_length and problem_count < 1000:
+        has_digit = any(col < len(line) and line[col].isdigit() for line in param_lines)
+        
+        if has_digit:
+            in_problem = True
+            for i in range(4):
+                if col < len(param_lines[i]) and param_lines[i][col].isdigit():
+                    current_problem[i] += param_lines[i][col]
+                else:
+                    current_problem[i] += '0'
+        else:
+            if in_problem:
+                for i in range(4):
+                    value = int(current_problem[i]) if current_problem[i] else 0
+                    all_values[i].append(value)
+                
+                problem_start = col - len(current_problem[0])
+                for op_col in range(problem_start, col):
+                    if op_col < len(operator_line) and operator_line[op_col] in ['*', '+']:
+                        operators.append(operator_line[op_col])
+                        break
+                
+                current_problem = ['', '', '', '']
+                in_problem = False
+                problem_count += 1
+        
+        col += 1
+    
+    if in_problem and any(current_problem):
+        for i in range(4):
+            value = int(current_problem[i]) if current_problem[i] else 0
+            all_values[i].append(value)
+        
+        problem_start = col - len(current_problem[0])
+        for op_col in range(problem_start, col):
+            if op_col < len(operator_line) and operator_line[op_col] in ['*', '+']:
+                operators.append(operator_line[op_col])
+                break
+        
+        problem_count += 1
+    
+    for i in range(4):
         with open(f'line{i+1}.mem', 'w') as f:
-            for num in numbers:
-                # Convert to hex format (4 digits) for Verilog $readmemh
-                f.write(f"{int(num):04X}\n")
-        print(f"Created line{i+1}.mem with {len(numbers)} elements")
+            for value in all_values[i]:
+                hex_value = hex(value)[2:]  
+                f.write(f"{hex_value}\n")
+        print(f"Created line{i+1}.mem with {len(all_values[i])} elements")
     
-    # Process line 5 (operators: + and *)
-    operators = lines[4].strip()
     with open('op.mem', 'w') as f:
-        for char in operators:
-            if char == '+':
+        for op in operators:
+            if op == '+':
                 f.write("1\n")
-            elif char == '*':
+            elif op == '*':
                 f.write("0\n")
-            # Skip spaces and other characters
     
-    # Count actual operators (not spaces)
-    op_count = sum(1 for char in operators if char in ['+', '*'])
-    print(f"Created op.mem with {op_count} elements")
+    print(f"Created op.mem with {len(operators)} elements")
     print("Conversion complete!")
 
 if __name__ == "__main__":

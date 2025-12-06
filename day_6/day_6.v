02923eb3dedcab8e8982f05962e30bc02aa520b0
TrungBui32 <thetrung205@gmail.com>
2025-12-07 00:59:53 +0800

    sequential solution part 2

--- day_6/day_6.v
+++ day_6/day_6.v
@@ -20,11 +20,16 @@ module day_6 (
     reg [2:0] state;
     reg [15:0] idx;
     reg [RESULT_WIDTH-1:0] sum_accumulator;
+    reg [DATA_WIDTH-1:0] temp_values [0:3];
+    reg [DATA_WIDTH-1:0] temp_values_1 [0:3];
+
     
     localparam IDLE = 3'd0;
-    localparam CALC = 3'd1;
-    localparam SUM = 3'd2;
-    localparam DONE = 3'd3;
+    localparam FORMAT = 3'd1;
+    localparam EXTRACT = 3'd2;
+    localparam CALC = 3'd3;
+    localparam SUM = 3'd4;
+    localparam DONE = 3'd5;
 
     initial begin
         $readmemh("line1.mem", line1);
@@ -45,24 +50,77 @@ module day_6 (
             case (state)
                 IDLE: begin
                     if (start) begin
-                        state <= CALC;
+                        state <= EXTRACT;
                         idx <= 0;
                         sum_accumulator <= 0;
                         finished <= 0;
                     end
                 end
+                EXTRACT: begin
+                    temp_values_1[0] = (line1[idx] % 10) * 1000 + (line2[idx] % 10) * 100 + (line3[idx] % 10) * 10 + (line4[idx] % 10);
+                    temp_values_1[1] = ((line1[idx] / 10) % 10) * 1000 + ((line2[idx] / 10) % 10) * 100 + ((line3[idx] / 10) % 10) * 10 + ((line4[idx] / 10) % 10);
+                    temp_values_1[2] = ((line1[idx] / 100) % 10) * 1000 + ((line2[idx] / 100) % 10) * 100 + ((line3[idx] / 100) % 10) * 10 + ((line4[idx] / 100) % 10);
+                    temp_values_1[3] = ((line1[idx] / 1000) % 10) * 1000 + ((line2[idx] / 1000) % 10) * 100 + ((line3[idx] / 1000) % 10) * 10 + ((line4[idx] / 1000) % 10);
+
+                    if(temp_values_1[0] % 1000 == 0) begin
+                        temp_values[0] = temp_values_1[0] / 1000;
+                    end else if (temp_values_1[0] % 100 == 0) begin
+                        temp_values[0] = temp_values_1[0] / 100;
+                    end else if (temp_values_1[0] % 10 == 0) begin
+                        temp_values[0] = temp_values_1[0] / 10;
+                    end else begin
+                        temp_values[0] = temp_values_1[0];
+                    end
+
+                    if(temp_values_1[1] % 1000 == 0) begin
+                        temp_values[1] = temp_values_1[1] / 1000;
+                    end else if (temp_values_1[1] % 100 == 0) begin
+                        temp_values[1] = temp_values_1[1] / 100;
+                    end else if (temp_values_1[1] % 10 == 0) begin
+                        temp_values[1] = temp_values_1[1] / 10;
+                    end else begin
+                        temp_values[1] = temp_values_1[1];
+                    end
+
+                    if(temp_values_1[2] % 1000 == 0) begin
+                        temp_values[2] = temp_values_1[2] / 1000;
+                    end else if (temp_values_1[2] % 100 == 0) begin
+                        temp_values[2] = temp_values_1[2] / 100;
+                    end else if (temp_values_1[2] % 10 == 0) begin
+                        temp_values[2] = temp_values_1[2] / 10;
+                    end else begin
+                        temp_values[2] = temp_values_1[2];
+                    end
+
+                    if(temp_values_1[3] % 1000 == 0) begin
+                        temp_values[3] = temp_values_1[3] / 1000;
+                    end else if (temp_values_1[3] % 100 == 0) begin
+                        temp_values[3] = temp_values_1[3] / 100;
+                    end else if (temp_values_1[3] % 10 == 0) begin
+                        temp_values[3] = temp_values_1[3] / 10;
+                    end else begin
+                        temp_values[3] = temp_values_1[3];
+                    end
+                    $display("Idx: %d, Values: %d, %d, %d, %d, Op: %b", idx, temp_values[0], temp_values[1], temp_values[2], temp_values[3], op[idx]);
+                    state <= CALC;
+                end
                 CALC: begin
                     if (op[idx] == 1'b0) begin 
-                        result_array[idx] <= line1[idx] * line2[idx] * line3[idx] * line4[idx];
+                        result_array[idx] = ((temp_values[0] != 0) ? temp_values[0] : 1) * 
+                                            ((temp_values[1] != 0) ? temp_values[1] : 1) * 
+                                            ((temp_values[2] != 0) ? temp_values[2] : 1) * 
+                                            ((temp_values[3] != 0) ? temp_values[3] : 1);
                     end else begin  
-                        result_array[idx] <= line1[idx] + line2[idx] + line3[idx] + line4[idx];
+                        result_array[idx] = temp_values[0] + temp_values[1] + temp_values[2] + temp_values[3];
                     end
                     if (idx == NUM_ELEMENTS - 1) begin
                         state <= SUM;
                         idx <= 0;
                     end else begin
                         idx <= idx + 1;
+                        state <= EXTRACT;
                     end
+                    $display("Idx: %d, Result: %d", idx, result_array[idx]);
                 end
                 SUM: begin
                     sum_accumulator <= sum_accumulator + result_array[idx];

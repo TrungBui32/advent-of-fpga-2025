7180c548cc986e82bb25801f2bcd248976e35a04
TrungBui32 <thetrung205@gmail.com>
2025-12-09 22:32:15 +0800

    add input and converter

---
+++ day_9/convert_input.py
@@ -0,0 +1,12 @@
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+with open('x.mem', 'w') as x_file, open('y.mem', 'w') as y_file:
+    for line in lines:
+        line = line.strip()
+        if line:  
+            x, y = line.split(',')
+            x_file.write(format(int(x), '064b') + '\n')
+            y_file.write(format(int(y), '064b') + '\n')
+
+print("Conversion complete! Created x.mem and y.mem with binary format")

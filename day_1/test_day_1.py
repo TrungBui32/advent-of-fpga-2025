46b626088a0eddf7f0e378abba8dd66edc502986
TrungBui32 <thetrung205@gmail.com>
2025-12-18 19:13:06 +0800

    update tb

--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,5 +1,5 @@
 import cocotb
-from cocotb.triggers import Timer, RisingEdge
+from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.clock import Clock
 
 @cocotb.test()
@@ -7,10 +7,15 @@ async def test(dut):
     clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
     
-    dut.process.value = 0
-    await Timer(100, units='ns')
+    dut.rst.value = 1
+    await RisingEdge(dut.clk)
+    dut.rst.value = 0
+    await RisingEdge(dut.clk)
     
-    dut.process.value = 1
+    dut.start.value = 1
+    await RisingEdge(dut.clk)
+    dut.start.value = 0
+    await RisingEdge(dut.clk)
     
     while not dut.finished.value:
         await RisingEdge(dut.clk)

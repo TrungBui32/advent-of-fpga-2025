d28e3eacdb3258456f6a77e4528f0a29edb53eaa
TrungBui32 <thetrung205@gmail.com>
2025-12-30 15:31:03 +0700

    optimize part 1

--- day_9/movie_theater_part1.v
+++ day_9/movie_theater_part1.v
@@ -8,95 +8,102 @@ module movie_theater_part1(
 
     localparam NUM_ELEMENTS = 496;
     localparam DATA_WIDTH = 64;
+    localparam NUM_ENGINES = 8;
 
-    localparam IDLE = 2'd0;
-    localparam PROCESS = 2'd1;
-    localparam DONE = 2'd2;
-    
     reg [DATA_WIDTH-1:0] x [0:NUM_ELEMENTS-1];
     reg [DATA_WIDTH-1:0] y [0:NUM_ELEMENTS-1];
 
     reg [1:0] state;
+    localparam IDLE = 2'd0;
+    localparam PROCESS = 2'd1;
+    localparam DRAIN = 2'd2;
+
     reg [15:0] i, j;
-    
-    reg [DATA_WIDTH-1:0] stage1_x_i, stage1_x_j;
-    reg [DATA_WIDTH-1:0] stage1_y_i, stage1_y_j;
-    reg stage1_valid;
-    
-    reg [DATA_WIDTH-1:0] stage2_dx, stage2_dy;
-    reg stage2_valid;
-    
-    reg [DATA_WIDTH-1:0] stage3_area;
-    reg stage3_valid;
-    
-    reg [DATA_WIDTH-1:0] largest_area;
-    
+    reg [63:0] largest_area;
+
+    reg [DATA_WIDTH-1:0] s1_xi, s1_yi;
+    reg [DATA_WIDTH-1:0] s1_xj [0:NUM_ENGINES-1];
+    reg [DATA_WIDTH-1:0] s1_yj [0:NUM_ENGINES-1];
+    reg [NUM_ENGINES-1:0] s1_valid;
+
+    reg [DATA_WIDTH-1:0] s2_dx [0:NUM_ENGINES-1];
+    reg [DATA_WIDTH-1:0] s2_dy [0:NUM_ENGINES-1];
+    reg [NUM_ENGINES-1:0] s2_valid;
+
+    reg [DATA_WIDTH-1:0] s3_area [0:NUM_ENGINES-1];
+    reg [NUM_ENGINES-1:0] s3_valid;
+
     initial begin
         $readmemb("x.mem", x);
         $readmemb("y.mem", y);
     end
 
     always @(posedge clk) begin
-        stage2_dx <= (stage1_x_i > stage1_x_j) ? (stage1_x_i - stage1_x_j) : (stage1_x_j - stage1_x_i);
-        stage2_dy <= (stage1_y_i > stage1_y_j) ? (stage1_y_i - stage1_y_j) : (stage1_y_j - stage1_y_i);
-        stage2_valid <= stage1_valid;
-            
-        stage3_area <= (stage2_dx + 1) * (stage2_dy + 1);
-        stage3_valid <= stage2_valid;
-            
-        if (stage3_valid && stage3_area > largest_area) begin
-            largest_area <= stage3_area;
+        for (k=0; k<NUM_ENGINES; k=k+1) begin
+            s2_dx[k] <= (s1_xi > s1_xj[k]) ? (s1_xi - s1_xj[k]) : (s1_xj[k] - s1_xi);
+            s2_dy[k] <= (s1_yi > s1_yj[k]) ? (s1_yi - s1_yj[k]) : (s1_yj[k] - s1_yi);
         end
-    end
+        s2_valid <= s1_valid;
 
+        for (k=0; k<NUM_ENGINES; k=k+1) begin
+            s3_area[k] <= (s2_dx[k] + 1) * (s2_dy[k] + 1);
+                
+            if (s2_valid[k] && (s3_area[k] > largest_area)) begin
+                largest_area <= s3_area[k];
+            end
+        end
+        s3_valid <= s2_valid;
+    end 
+
+    integer k;
     always @(posedge clk or posedge rst) begin
         if (rst) begin
+            state <= IDLE;
             finished <= 0;
             result <= 0;
-            state <= IDLE;
             largest_area <= 0;
-            i <= 0;
-            j <= 0;
-            stage1_valid <= 0;
-            stage2_valid <= 0;
-            stage3_valid <= 0;
-        end else begin            
+            s1_valid <= 0; s2_valid <= 0; s3_valid <= 0;
+        end else begin
             case (state)
                 IDLE: begin
                     if (start) begin
                         state <= PROCESS;
-                        i <= 0;
-                        j <= 1;
+                        i <= 0; j <= 1;
                         largest_area <= 0;
                         finished <= 0;
                     end
+                    s1_valid <= 0;
                 end
                 PROCESS: begin
-                    stage1_x_i <= x[i];
-                    stage1_x_j <= x[j];
-                    stage1_y_i <= y[i];
-                    stage1_y_j <= y[j];
-                    stage1_valid <= 1;
-                    
-                    if (j < NUM_ELEMENTS - 1) begin
-                        j <= j + 1;
+                    s1_xi <= x[i];
+                    s1_yi <= y[i];
+                    for (k=0; k<NUM_ENGINES; k=k+1) begin
+                        if (j + k < NUM_ELEMENTS) begin
+                            s1_xj[k] <= x[j+k];
+                            s1_yj[k] <= y[j+k];
+                            s1_valid[k] <= 1'b1;
+                        end else begin
+                            s1_valid[k] <= 1'b0;
+                        end
+                    end
+
+                    if (j + NUM_ENGINES < NUM_ELEMENTS) begin
+                        j <= j + NUM_ENGINES;
                     end else begin
                         if (i < NUM_ELEMENTS - 2) begin
                             i <= i + 1;
                             j <= i + 2;
                         end else begin
-                            state <= DONE;
-                            stage1_valid <= 0;
+                            state <= DRAIN;
                         end
                     end
                 end
-                DONE: begin
-                    if (!stage1_valid && !stage2_valid && !stage3_valid) begin
+                DRAIN: begin
+                    s1_valid <= 0;
+                    if (s2_valid == 0 && s3_valid == 0) begin
                         finished <= 1;
                         result <= largest_area;
-                        if (!start) begin
-                            state <= IDLE;
-                        end
+                        if (!start) state <= IDLE;
                     end
                 end
             endcase

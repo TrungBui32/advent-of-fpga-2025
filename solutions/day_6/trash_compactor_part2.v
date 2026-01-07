3210bc94235ac248aeb6c35b0156c210e77639a9
TrungBui32 <thetrung205@gmail.com>
2026-01-07 21:59:14 +0700

    pipelined version of part 2

--- solutions/day_6/trash_compactor_part2.v
+++ solutions/day_6/trash_compactor_part2.v
@@ -1,180 +1,193 @@
 module trash_compactor_part2 (
     input clk,
     input rst,
-    input [31:0] data_in,
+    input [31:0] data_in,  
+    input op,
     input valid_in,
     output ready,
     output reg finished,
     output reg [63:0] result  
 );
     localparam DATA_WIDTH = 16;
-    localparam NUM_ELEMENTS = 1000;  
     localparam RESULT_WIDTH = 64;
-    localparam PARALLEL = 25;  
+    localparam NUM_ELEMENTS = 1000;
+    localparam CHUNK1 = 1'b0;
+    localparam CHUNK2 = 1'b1;
     
-    reg [DATA_WIDTH-1:0] line1 [0:NUM_ELEMENTS-1];
-    reg [DATA_WIDTH-1:0] line2 [0:NUM_ELEMENTS-1];
-    reg [DATA_WIDTH-1:0] line3 [0:NUM_ELEMENTS-1];
-    reg [DATA_WIDTH-1:0] line4 [0:NUM_ELEMENTS-1];
-    reg op [0:NUM_ELEMENTS-1];
+    reg word_cnt;
+    reg [63:0] buffer;
+    reg input_ready;
+    reg buffer_op;
     
-    reg [2:0] state;
-    reg [15:0] idx [0:PARALLEL-1];
+    reg stage1_valid;
+    reg [DATA_WIDTH-1:0] stage1_line1;
+    reg [DATA_WIDTH-1:0] stage1_line2;
+    reg [DATA_WIDTH-1:0] stage1_line3;
+    reg [DATA_WIDTH-1:0] stage1_line4;
+    reg stage1_op;
+    
+    reg stage2_valid;
+    reg [RESULT_WIDTH-1:0] stage2_result1;
+    reg [DATA_WIDTH-1:0] stage2_line3;
+    reg [DATA_WIDTH-1:0] stage2_line4;
+    reg stage2_op;
+    
+    reg stage3_valid;
+    reg [RESULT_WIDTH-1:0] stage3_result2;
+    reg [DATA_WIDTH-1:0] stage3_line4;
+    reg stage3_op;
+    
+    reg stage4_valid;
+    reg [RESULT_WIDTH-1:0] stage4_final;
+    
     reg [RESULT_WIDTH-1:0] sum_accumulator;
+    reg [31:0] count_valid;
     
-    wire [DATA_WIDTH-1:0] temp_values [0:PARALLEL-1][0:3];
-    reg [DATA_WIDTH-1:0] temp_storage [0:PARALLEL-1][0:3];
-    reg [63:0] saved_temp [0:PARALLEL*2-1];
+    assign ready = 1'b1;
     
-    wire [3:0] line_digits [0:3][0:3][0:PARALLEL-1]; 
-    wire [15:0] bcd_values [0:PARALLEL-1][0:3];
-    wire [DATA_WIDTH-1:0] extracted_values [0:PARALLEL-1][0:3][0:3]; 
-    
-    localparam IDLE = 3'd0;
-    localparam EXTRACT = 3'd1;
-    localparam CALC_1 = 3'd2;
-    localparam CALC_2 = 3'd3;
-    localparam CALC_3 = 3'd4;
-    localparam DONE = 3'd5;
-
-    initial begin
-        $readmemh("line1.mem", line1);
-        $readmemh("line2.mem", line2);
-        $readmemh("line3.mem", line3);
-        $readmemh("line4.mem", line4);
-        $readmemh("op.mem", op);
-    end
-
-    genvar p, d, v;
-    generate
-        for (p = 0; p < PARALLEL; p = p + 1) begin : parallel_gen
-            assign line_digits[0][0][p] = line1[idx[p]][3:0];
-            assign line_digits[0][1][p] = line1[idx[p]][7:4];
-            assign line_digits[0][2][p] = line1[idx[p]][11:8];
-            assign line_digits[0][3][p] = line1[idx[p]][15:12];
-            
-            assign line_digits[1][0][p] = line2[idx[p]][3:0];
-            assign line_digits[1][1][p] = line2[idx[p]][7:4];
-            assign line_digits[1][2][p] = line2[idx[p]][11:8];
-            assign line_digits[1][3][p] = line2[idx[p]][15:12];
-            
-            assign line_digits[2][0][p] = line3[idx[p]][3:0];
-            assign line_digits[2][1][p] = line3[idx[p]][7:4];
-            assign line_digits[2][2][p] = line3[idx[p]][11:8];
-            assign line_digits[2][3][p] = line3[idx[p]][15:12];
-            
-            assign line_digits[3][0][p] = line4[idx[p]][3:0];
-            assign line_digits[3][1][p] = line4[idx[p]][7:4];
-            assign line_digits[3][2][p] = line4[idx[p]][11:8];
-            assign line_digits[3][3][p] = line4[idx[p]][15:12];
-            
-            for (d = 0; d < 4; d = d + 1) begin : digit_gen
-                assign bcd_values[p][d] = {line_digits[0][d][p], line_digits[1][d][p], 
-                                           line_digits[2][d][p], line_digits[3][d][p]};
-                
-                assign extracted_values[p][d][3] = line_digits[0][d][p];
-                assign extracted_values[p][d][2] = (line_digits[0][d][p] << 3) + (line_digits[0][d][p] << 1) + line_digits[1][d][p];
-                assign extracted_values[p][d][1] = (line_digits[0][d][p] << 6) + (line_digits[0][d][p] << 5) + (line_digits[0][d][p] << 2) + 
-                                                   (line_digits[1][d][p] << 3) + (line_digits[1][d][p] << 1) + line_digits[2][d][p];
-                assign extracted_values[p][d][0] = (line_digits[0][d][p] << 9) + (line_digits[0][d][p] << 8) + (line_digits[0][d][p] << 7) + 
-                                                   (line_digits[0][d][p] << 6) + (line_digits[0][d][p] << 5) + (line_digits[0][d][p] << 3) +
-                                                   (line_digits[1][d][p] << 6) + (line_digits[1][d][p] << 5) + (line_digits[1][d][p] << 2) + 
-                                                   (line_digits[2][d][p] << 3) + (line_digits[2][d][p] << 1) + line_digits[3][d][p];
-                
-                assign temp_values[p][d] = (bcd_values[p][d][11:0] == 0) ? extracted_values[p][d][3] :
-                                           (bcd_values[p][d][7:0] == 0) ? extracted_values[p][d][2] :
-                                           (bcd_values[p][d][3:0] == 0) ? extracted_values[p][d][1] :
-                                           extracted_values[p][d][0];
+    function [15:0] bcd_to_binary;
+        input [3:0] d3, d2, d1, d0;
+        begin
+            if(d2 == 0 && d1 == 0 && d0 == 0) begin
+                bcd_to_binary = d3;
+            end else if(d1 == 0 && d0 == 0) begin
+                bcd_to_binary = d3 * 10 + d2;
+            end else if(d0 == 0) begin
+                bcd_to_binary = d3 * 100 + d2 * 10 + d1;
+            end else begin
+                bcd_to_binary = d3 * 1000 + d2 * 100 + d1 * 10 + d0;
             end
         end
-    endgenerate
-
-    reg [63:0] parallel_sum;
-
-    integer j;
-    always @(*) begin
-        parallel_sum = 0;
-        for (j = 0; j < PARALLEL; j = j + 1) begin
-            parallel_sum = parallel_sum + saved_temp[j*2];
+    endfunction
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            word_cnt <= CHUNK1;
+            input_ready <= 0;
+            buffer <= 64'b0;
+            buffer_op <= 0;
+        end else begin
+            input_ready <= 0;
+            if (valid_in) begin
+                if (word_cnt == CHUNK1) begin
+                    buffer[31:0] <= data_in;
+                    buffer_op <= op;
+                    word_cnt <= CHUNK2;
+                end else begin
+                    buffer[63:32] <= data_in;
+                    word_cnt <= CHUNK1;
+                    input_ready <= 1;
+                end
+            end
         end
     end
-
-    integer i;
-    always @(posedge clk or posedge rst) begin
+    
+    always @(posedge clk) begin
         if (rst) begin
-            state <= IDLE;
-            for (i = 0; i < PARALLEL; i = i + 1) idx[i] <= 0;
+            stage1_valid <= 0;
+        end else begin
+            stage1_valid <= input_ready;
+            if (input_ready) begin
+                stage1_line1 <= bcd_to_binary(buffer[3:0], buffer[19:16], buffer[35:32], buffer[51:48]);
+                stage1_line2 <= bcd_to_binary(buffer[7:4], buffer[23:20], buffer[39:36], buffer[55:52]);
+                stage1_line3 <= bcd_to_binary(buffer[11:8], buffer[27:24], buffer[43:40], buffer[59:56]);
+                stage1_line4 <= bcd_to_binary(buffer[15:12], buffer[31:28], buffer[47:44], buffer[63:60]);
+                stage1_op <= buffer_op;
+            end
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            stage2_valid <= 0;
+            stage2_result1 <= 0;
+        end else begin
+            stage2_valid <= stage1_valid;
+            if (stage1_valid) begin
+                if (stage1_op == 1'b0) begin 
+                    if(stage1_line1 != 0 && stage1_line2 != 0) begin
+                        stage2_result1 <= stage1_line1 * stage1_line2;
+                    end else if(stage1_line1 == 0) begin
+                        stage2_result1 <= stage1_line2;
+                    end else if(stage1_line2 == 0) begin
+                        stage2_result1 <= stage1_line1;
+                    end else begin
+                        stage2_result1 <= 1;
+                    end
+                end else begin
+                    stage2_result1 <= stage1_line1 + stage1_line2;
+                end
+                stage2_line3 <= stage1_line3;
+                stage2_line4 <= stage1_line4;
+                stage2_op <= stage1_op;
+            end
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            stage3_valid <= 0;
+            stage3_result2 <= 0;
+        end else begin
+            stage3_valid <= stage2_valid;
+            if (stage2_valid) begin
+                if (stage2_op == 1'b0) begin
+                    if(stage2_result1 != 0 && stage2_line3 != 0) begin
+                        stage3_result2 <= stage2_result1 * stage2_line3;
+                    end else if(stage2_result1 == 0) begin
+                        stage3_result2 <= stage2_line3;
+                    end else if(stage2_line3 == 0) begin
+                        stage3_result2 <= stage2_result1;
+                    end else begin
+                        stage3_result2 <= 1;
+                    end
+                end else begin
+                    stage3_result2 <= stage2_result1 + stage2_line3;
+                end
+                stage3_line4 <= stage2_line4;
+                stage3_op <= stage2_op;
+            end
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            stage4_valid <= 0;
+            stage4_final <= 0;
+        end else begin
+            stage4_valid <= stage3_valid;
+            if (stage3_valid) begin
+                if (stage3_op == 1'b0) begin
+                    if(stage3_result2 != 0 && stage3_line4 != 0) begin
+                        stage4_final <= stage3_result2 * stage3_line4;
+                    end else if(stage3_result2 == 0) begin
+                        stage4_final <= stage3_line4;
+                    end else if(stage3_line4 == 0) begin
+                        stage4_final <= stage3_result2;
+                    end else begin
+                        stage4_final <= 1;
+                    end
+                end else begin
+                    stage4_final <= stage3_result2 + stage3_line4;
+                end
+            end
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (rst) begin
             sum_accumulator <= 0;
+            count_valid <= 0;
             finished <= 0;
             result <= 0;
-        end else begin
-            case (state)
-                IDLE: begin
-                    if (start) begin
-                        state <= EXTRACT;
-                        for (i = 0; i < PARALLEL; i = i + 1) idx[i] <= i;
-                        sum_accumulator <= 0;
-                        finished <= 0;
-                    end
-                end
-                
-                EXTRACT: begin
-                    for (i = 0; i < PARALLEL; i = i + 1) begin
-                        if (op[idx[i]] == 1'b0) begin
-                            temp_storage[i][0] <= (temp_values[i][0] != 0) ? temp_values[i][0] : 1;
-                            temp_storage[i][1] <= (temp_values[i][1] != 0) ? temp_values[i][1] : 1;
-                            temp_storage[i][2] <= (temp_values[i][2] != 0) ? temp_values[i][2] : 1;
-                            temp_storage[i][3] <= (temp_values[i][3] != 0) ? temp_values[i][3] : 1;
-                        end else begin
-                            temp_storage[i][0] <= temp_values[i][0];
-                            temp_storage[i][1] <= temp_values[i][1];
-                            temp_storage[i][2] <= temp_values[i][2];
-                            temp_storage[i][3] <= temp_values[i][3];
-                        end
-                    end
-                    state <= CALC_1;
-                end
-                
-                CALC_1: begin
-                    for (i = 0; i < PARALLEL; i = i + 1) begin
-                        if (op[idx[i]] == 1'b0) begin
-                            saved_temp[i*2] <= temp_storage[i][0] * temp_storage[i][1];
-                            saved_temp[i*2+1] <= temp_storage[i][2] * temp_storage[i][3];
-                        end else begin
-                            saved_temp[i*2] <= temp_storage[i][0] + temp_storage[i][1];
-                            saved_temp[i*2+1] <= temp_storage[i][2] + temp_storage[i][3];
-                        end
-                    end
-                    state <= CALC_2;
-                end
-                
-                CALC_2: begin
-                    for (i = 0; i < PARALLEL; i = i + 1) begin
-                        if (op[idx[i]] == 1'b0)
-                            saved_temp[i*2] <= saved_temp[i*2] * saved_temp[i*2+1];
-                        else
-                            saved_temp[i*2] <= saved_temp[i*2] + saved_temp[i*2+1];
-                    end
-                    state <= CALC_3;
-                end
-                
-                CALC_3: begin
-                    sum_accumulator <= sum_accumulator + parallel_sum;
-                    
-                    if (idx[PARALLEL-1] >= NUM_ELEMENTS - 1) begin
-                        state <= DONE;
-                    end else begin
-                        for (i = 0; i < PARALLEL; i = i + 1) idx[i] <= idx[i] + PARALLEL;
-                        state <= EXTRACT;
-                    end
-                end
-                
-                DONE: begin
-                    result <= sum_accumulator;
-                    finished <= 1;
-                end
-            endcase
+        end else if (stage4_valid) begin
+            sum_accumulator <= sum_accumulator + stage4_final;
+            count_valid <= count_valid + 1;
+            
+            if (count_valid == NUM_ELEMENTS - 1) begin
+                finished <= 1;
+                result <= sum_accumulator + stage4_final;
+            end
         end
     end
-
 endmodule
